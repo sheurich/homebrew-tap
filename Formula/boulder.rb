f163ba891018ecd3fa42f5e6cede499d2433402c
Shiloh Heurich <sheurich@fastly.com>
2022-12-19 17:56:25 -0500

    Update version and add livecheck.

--- Formula/boulder.rb
+++ Formula/boulder.rb
@@ -4,11 +4,16 @@ class Boulder < Formula
   url "https://github.com/letsencrypt/boulder.git",
     tag:      "release-2022-12-12",
     revision: "fe2cf7d13695b9f3de2a2b47aa735fa50a0e2100"
-  version "release-2022-12-12"
   license "MPL-2.0"
+ 
   head "https://github.com/letsencrypt/boulder.git",
     branch: "main"
 
+  livecheck do
+    url :stable
+    regex(/^release-?(\d+(-\d+)+)$/i)
+  end
+  
   depends_on "go" => :build
 
   def install
