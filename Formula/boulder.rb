0f7c718d086d3baf1cf986c4001b3ebfc8686e49
Shiloh Heurich <sheurich@fastly.com>
2022-12-19 17:59:24 -0500

    style

--- Formula/boulder.rb
+++ Formula/boulder.rb
@@ -5,7 +5,7 @@ class Boulder < Formula
     tag:      "release-2022-12-12",
     revision: "fe2cf7d13695b9f3de2a2b47aa735fa50a0e2100"
   license "MPL-2.0"
- 
+
   head "https://github.com/letsencrypt/boulder.git",
     branch: "main"
 
@@ -13,7 +13,7 @@ class Boulder < Formula
     url :stable
     regex(/^release-?(\d+(-\d+)+)$/i)
   end
-  
+
   depends_on "go" => :build
 
   def install
