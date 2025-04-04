6e50322d18bf97bfb00f065535d92781d58ec9d4
Shiloh Heurich <sheurich@users.noreply.github.com>
2025-04-04 00:04:06 +0000

    boulder 2025-04-03

--- Formula/boulder.rb
+++ Formula/boulder.rb
@@ -2,8 +2,8 @@ class Boulder < Formula
   desc "ACME-based certificate authority, written in Go"
   homepage "https://github.com/letsencrypt/boulder"
   url "https://github.com/letsencrypt/boulder.git",
-    tag:      "release-2025-03-31",
-    revision: "3438b057d82576f93dbda642869f5259f5623ba9"
+    tag:      "release-2025-04-03",
+    revision: "27e08f4846985c9d49f9f2bb5d3c9006383c5c97"
   license "MPL-2.0"
 
   head "https://github.com/letsencrypt/boulder.git",
